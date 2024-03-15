45a30be27aa7bbb4c3f1bf1f60f561b7c43ba02e
Haresz <rizalfikri852@gmail.com>
2024-03-15 15:13:53 +0700

    coba optimasi componet hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,15 +1,34 @@
+"use client";
 import { Box, Image } from "@chakra-ui/react";
-import React from "react";
+import { useEffect, useState } from "react";
 
 export default function Hero(props: any) {
+  const [rendered, setRendered] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setRendered(true);
+    }, 1000);
+
+    return () => clearTimeout(timer);
+  }, []);
   return (
-    <Image
-      boxSize="full"
-      src={props.src}
-      alt="Dan Abramov"
-      fallback={
+    <>
+      {rendered ? (
+        <Image
+          boxSize="full"
+          src={props.src}
+          alt="Dan Abramov"
+          fallback={
+            <Box className="w-full lg:h-96 h-64 bg-gray-400 animate-pulse" />
+          }
+        />
+      ) : (
         <Box className="w-full lg:h-96 h-64 bg-gray-400 animate-pulse" />
-      }
-    />
+      )}
+    </>
   );
 }
+
+// export const Hero = (props: any) =>
+//   setTimeout(() => HeroRender(props.src), 1000);
