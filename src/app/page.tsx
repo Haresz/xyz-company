abe596b2d2b40fda9757f92c602da04f45b02605
Haresz <rizalfikri852@gmail.com>
2024-03-17 16:59:21 +0700

    cobak optimasi home mobile dari chat gbt

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,32 +1,25 @@
 "use client";
-import React, { useEffect, useRef } from "react";
+import React, { useRef } from "react";
 import { Box, Image, Text } from "@chakra-ui/react";
 import Marquee from "react-fast-marquee";
 import Title from "@/components/Title";
 import OurTeams from "@/components/OurTeams";
 import { actionGetTeams } from "@/lib/features/teams/teamsSlice";
 import { RootState } from "@/lib/store";
-import { useSelector, useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
 import { useAppStore } from "@/lib/hooks";
 import Testimoni from "@/components/Testimoni";
 import OurServises from "@/components/OurServises";
 import Hero from "@/components/Hero";
 
-const IMAGE_SIZE = "16%";
-const INITIAL_TEAM_COUNT = 7;
-
 export default function Home() {
-  const teams = useSelector((state: RootState) => state.teamsSlice.teams);
-  const dispatch = useDispatch();
+  const teams: any = useSelector((state: RootState) => state.teamsSlice.teams);
   const store = useAppStore();
-  const initialized = useRef(false);
-
-  useEffect(() => {
-    if (!initialized.current) {
-      store.dispatch(actionGetTeams(16));
-      initialized.current = true;
-    }
-  }, [dispatch]);
+  const initialized: any = useRef(false);
+  if (!initialized) {
+    store.dispatch(actionGetTeams(16));
+    initialized.current = true;
+  }
 
   return (
     <div>
@@ -36,7 +29,7 @@ export default function Home() {
           <Text
             fontWeight={"semibold"}
             textAlign={"justify"}
-            className="md:w-8/12 w-full lg:px-8 px-4 mx-auto md:py-10 py-5 border--primary md:text-3xl text-md"
+            className="md:w-8/12 w-full lg:px-8 px-4 mx-auto md:py-10 py-5 border--primary md:text-3xl text-lg"
           >
             &quot;At XYZ Company, we&apos;ve been leading the industry since
             2005 with a dedicated team of experts in technology and customer
@@ -45,7 +38,7 @@ export default function Home() {
           </Text>
           <Image
             className="hidden lg:inline-block"
-            width={IMAGE_SIZE}
+            width={"16%"}
             src="founder.webp"
             alt="Dan Abramov"
           />
@@ -55,15 +48,18 @@ export default function Home() {
       <div className="our-teams mt-24">
         <Title title="Our Teams" />
         <Marquee className="our-teams flex gap-12 overflow-x-scroll pb-8 mt-16">
-          {teams
-            ?.slice(0, INITIAL_TEAM_COUNT)
-            .map((item: any, index: number) => (
-              <OurTeams
-                key={index}
-                img={item.picture.large}
-                name={`${item.name.first} ${item.name.last}`}
-              />
-            ))}
+          {teams.map((item: any, index: number) => {
+            console.log(index);
+            if (index < 7) {
+              return (
+                <OurTeams
+                  key={index}
+                  img={item.picture.large}
+                  name={`${item.name.first} ${item.name.first} ${item.name.last}`}
+                />
+              );
+            }
+          })}
         </Marquee>
         <Testimoni datas={teams} />
         <Text
